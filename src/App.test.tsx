27f3997b08a2a6f7867a4605044dbed481e38ec4
niknikolaou <83726482+niknikolaou@users.noreply.github.com>
2022-12-31 11:28:12 +0000

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { Mainnet, Config, MetamaskConnector } from '@usedapp/core'
 
-const config: Config = {
-  readOnlyChainId: Mainnet.chainId,
-  readOnlyUrls: {
-    [Mainnet.chainId]: 'https://polygon-mumbai.gateway.pokt.network/v1/lb/6312338e22a38a0039f241db',
-  },
-  connectors: {
-    metamask: new MetamaskConnector(),
-  }
-}
+
+
 
 test('renders learn react link', () => {
   render(<App />);
